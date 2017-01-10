d4ea8ab34d15892a2a31f03c913dedc16d0d8ae6
Jacob Landman <jakeypoolandman7@gmail.com>
2017-01-10 17:08:04 -0600

    Finished swapping screens app

---
+++ SwappingScreens/SwappingScreens/MusicListVC.swift
@@ -0,0 +1,37 @@
+//
+//  MusicListVC.swift
+//  SwappingScreens
+//
+//  Created by Jacob Landman on 1/10/17.
+//  Copyright © 2017 Jacob Landman. All rights reserved.
+//
+
+import UIKit
+
+class MusicListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor.blue
+    }
+    
+    @IBAction func backBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func load3rdScreenPressed(_ sender: Any) {
+        let songTitle = "Quit Playing Games With My Heart"
+        performSegue(withIdentifier: "PlaySongVC", sender: songTitle)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? PlaySongVC {
+            if let song = sender as? String {
+                destination.selectedSong = song
+            }
+        }
+    }
+    
+    
+}
