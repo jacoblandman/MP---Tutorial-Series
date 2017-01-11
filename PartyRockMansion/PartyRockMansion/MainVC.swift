6e0169327bed70e1924de52b76a30cc55b774fe0
Jacob Landman <jakeypoolandman7@gmail.com>
2017-01-11 16:36:09 -0600

    Finished UI Design of the PartyRockMansion App

---
+++ PartyRockMansion/PartyRockMansion/MainVC.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  PartyRockMansion
+//
+//  Created by Jacob Landman on 1/11/17.
+//  Copyright © 2017 Jacob Landman. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
---
+++ PartyRockMansion/PartyRockMansion/VideoVC.swift
@@ -0,0 +1,19 @@
+//
+//  VideoVC.swift
+//  PartyRockMansion
+//
+//  Created by Jacob Landman on 1/11/17.
+//  Copyright © 2017 Jacob Landman. All rights reserved.
+//
+
+import UIKit
+
+class VideoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
