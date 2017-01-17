b583f4f7280343b911432992f300a3b4878ad887
Jacob Landman <jakeypoolandman7@gmail.com>
2017-01-16 18:30:03 -0600

    added ui for the detail view controller

--- pokedex/pokedex/PokemonDetailVC.swift
+++ pokedex/pokedex/PokemonDetailVC.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class PokemonDetailVC: UIViewController {
 
+    var pokemon: Pokemon!
+    @IBOutlet weak var nameLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        nameLbl.text = pokemon.name
         
     }
 }
