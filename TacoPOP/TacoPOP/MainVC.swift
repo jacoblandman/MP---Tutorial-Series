ead5bc0152d048b80332a4f67aa97e0771fc5f60
Jacob Landman <jakeypoolandman7@gmail.com>
2017-01-17 13:27:10 -0600

    Initial Commit for TacoPOP app

---
+++ TacoPOP/TacoPOP/MainVC.swift
@@ -0,0 +1,23 @@
+//
+//  MainVC.swift
+//  TacoPOP
+//
+//  Created by Jacob Landman on 1/17/17.
+//  Copyright © 2017 Jacob Landman. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
