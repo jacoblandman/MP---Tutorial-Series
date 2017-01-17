97a0640044e1a3c074c3a9390e4a22d5bf64d741
Jacob Landman <jakeypoolandman7@gmail.com>
2017-01-16 19:07:16 -0600

    about to try to install swiftyJSON pod

---
+++ pokedex/pokedex/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  pokedex
+//
+//  Created by Jacob Landman on 1/16/17.
+//  Copyright © 2017 Jacob Landman. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
--- pokedex/pokedex/Pokemon.swift
+++ pokedex/pokedex/Pokemon.swift
@@ -7,11 +7,20 @@
 //
 
 import Foundation
+import Alamofire
 
 class Pokemon {
     
     private var _name: String!
     private var _pokedexId: Int!
+    private var _description: String!
+    private var _type: String!
+    private var _defense: String!
+    private var _height: String!
+    private var _weight: String!
+    private var _attack: String!
+    private var _nextEvolutionTxt: String!
+    private var _pokemonURL: String!
     
     var name: String {
         return _name
@@ -24,6 +33,23 @@ class Pokemon {
     init(name: String, pokedexId: Int) {
         self._name = name
         self._pokedexId = pokedexId
+        
+        self._pokemonURL = "\(URL_BASE)\(URL_POKEMON)\(pokedexId)"
     }
     
+    func downloadPokemonDetails(completed: @escaping DownloadComplete) {
+        
+        Alamofire.request(_pokemonURL).validate().responseJSON { (response) in
+            
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print(JSON: \(json))
+            case .failure(let error):
+                print(error)
+            }
+            let dict = response.result.value as? Dictionary<String, AnyObject>
+
+        }
+    }
 }
