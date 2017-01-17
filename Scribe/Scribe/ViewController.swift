363a4d30dba8651b688d4b43fb1a2336a7496700
Jacob Landman <jakeypoolandman7@gmail.com>
2017-01-17 16:15:08 -0600

    Finsihed speech recognition app

--- Scribe/Scribe/ViewController.swift
+++ Scribe/Scribe/ViewController.swift
@@ -7,19 +7,63 @@
 //
 
 import UIKit
+import Speech
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
 
+    @IBOutlet weak var activitySpinner: UIActivityIndicatorView!
+    @IBOutlet weak var transcriptionTextField: UITextView!
+    
+    var audioPlayer: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        activitySpinner.isHidden = true
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func requestSpeechAuth() {
+        SFSpeechRecognizer.requestAuthorization { authStatus in
+            if authStatus == SFSpeechRecognizerAuthorizationStatus.authorized {
+                if let path = Bundle.main.path(forResource: "test", ofType: "m4a") {
+                    do {
+                        let sound = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+                        self.audioPlayer = sound
+                        self.audioPlayer.delegate = self
+                        sound.play()
+                    } catch {
+                        print("Error!")
+                    }
+                    
+                    let recognizer = SFSpeechRecognizer()
+                    let request = SFSpeechURLRecognitionRequest(url: URL(fileURLWithPath: path))
+                    recognizer?.recognitionTask(with: request) { (result, error) in
+                        
+                        if let error = error  {
+                            print("There was an error: \(error)")
+                        } else {
+                            self.transcriptionTextField.text = result!.bestTranscription.formattedString
+                        }
+                    }
+                }
+            }
+        }
     }
-
-
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        if player == audioPlayer {
+            player.stop()
+            activitySpinner.stopAnimating()
+            activitySpinner.isHidden = true
+        }
+    }
+    
+    @IBAction func playBtnPressed(_ sender: Any) {
+        activitySpinner.isHidden = false
+        activitySpinner.startAnimating()
+        requestSpeechAuth()
+    }
+    
 }
 
