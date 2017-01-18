153ffe899700c06aa26077a18705ad58818ce68f
Jacob Landman <jakeypoolandman7@gmail.com>
2017-01-18 14:35:15 -0600

    Initial commit for pokefinder app

---
+++ pokefinder/pokefinder/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  pokefinder
+//
+//  Created by Jacob Landman on 1/18/17.
+//  Copyright © 2017 Jacob Landman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var geoFire: GeoFire!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ pokefinder/pokefinder/pokefinder-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  pokefinder-Bridging-Header.h
+//  pokefinder
+//
+//  Created by Jacob Landman on 1/18/17.
+//  Copyright © 2017 Jacob Landman. All rights reserved.
+//
+
+#import "GeoFire.h"
