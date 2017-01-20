a6382463c2f2143302428f439d00a0d519081cd2
Jacob Landman <jakeypoolandman7@gmail.com>
2017-01-20 17:02:53 -0600

    Added email login for the app

--- social/social/SignInVC.swift
+++ social/social/SignInVC.swift
@@ -13,6 +13,9 @@ import Firebase
 
 class SignInVC: UIViewController {
 
+    @IBOutlet weak var emailField: FancyField!
+    @IBOutlet weak var pwdField: FancyField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -50,6 +53,35 @@ class SignInVC: UIViewController {
             }
         })
     }
-
+    
+    // need to look into the authenication errors
+    // and account for all that could happen when trying to log in
+    @IBAction func signInTapped(_ sender: Any) {
+        
+        guard let email = emailField.text, !email.isEmpty else {
+            print("The email field needs to be populated")
+            return
+        }
+        
+        guard let pwd = pwdField.text, pwd.characters.count > 6 else {
+            print("The password field needs to be populated with more than 6 characters")
+            return
+        }
+        
+        FIRAuth.auth()?.signIn(withEmail: email, password: pwd, completion: { (user, error) in
+            if error == nil {
+                print("JACOB: Email user authenticated with Firebase")
+            } else {
+                // the user may not exist
+                FIRAuth.auth()?.createUser(withEmail: email, password: pwd, completion: { (user, error) in
+                    if error != nil {
+                        print("JACOB: Unable to authenticate email login with Firebase")
+                    } else {
+                        print("JACOB: Successfully authenticated new email user with Firebase")
+                    }
+                })
+            }
+        })
+    }
 }
 
