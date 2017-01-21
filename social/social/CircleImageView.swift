fc2f3f8b9d68837727fe3d000cfaa9649766311c
Jacob Landman <jakeypoolandman7@gmail.com>
2017-01-20 19:40:54 -0600

    finished ui for the feed view controller

--- social/social/CircleImageView.swift
+++ social/social/CircleImageView.swift
@@ -1,5 +1,5 @@
 //
-//  FancyView.swift
+//  CircleImageView.swift
 //  social
 //
 //  Created by Jacob Landman on 1/20/17.
@@ -8,16 +8,17 @@
 
 import UIKit
 
-class FancyView: UIView {
+class CircleImageView: UIImageView {
 
     override func awakeFromNib() {
-        super.awakeFromNib()
-        
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: SHADOW_GRAY).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
+    }
+    
+    override func layoutSubviews() {
+        layer.cornerRadius = self.frame.width / 2.0
     }
 
 }
