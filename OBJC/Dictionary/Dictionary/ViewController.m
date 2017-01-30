9560cda863a5ac42821aa59949cd8961f8ba52fa
Jacob Landman <jakeypoolandman7@gmail.com>
2017-01-30 13:56:45 -0600

    Adding dictionary tutorial

--- OBJC/Dictionary/Dictionary/ViewController.m
+++ OBJC/Dictionary/Dictionary/ViewController.m
@@ -16,13 +16,28 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+    NSNumber *age = [NSNumber numberWithInt:40];
+    NSNumber *age2 = [NSNumber numberWithInt:34];
+    
+    // this is bad practive, but you can mix data types
+    //NSDictionary *dict = @{@"jack": age, @"jon": age2, @"peter": @"parker"};
+    
+    NSDictionary *dict = @{@"jack": age, @"jon": age2};
+    
+    int jacksAge = [[dict objectForKey:@"jack"] intValue];
+    NSLog(@"Jacks age: %d", jacksAge);
+    
+    // literal syntax grabs a regular deictionary,
+    // then you have to call the mutable copy function
+    NSMutableDictionary *mut = [@{@"1": @"BMW", @"2": @"Volvo", @"3": @"Ugly Honda Civic"} mutableCopy];
+    
+    // the better way to do it
+    NSMutableDictionary *mut2 = [[NSMutableDictionary alloc]init];
+    
+    [mut2 setObject:@"An Object" forKey:@"A key"];
+    [mut2 setObject:@"An Object 2" forKey:@"A second key"];
+    
 }
 
 
