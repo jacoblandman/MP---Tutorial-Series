90ce88aa22c6f17e4472a0a29481d1404e6c1b39
Jacob Landman <jakeypoolandman7@gmail.com>
2017-02-01 11:33:49 -0600

    Finish app by adding Video VC and adding css to style it

---
+++ DevslopesTutorials/DevslopesTutorials/VideoVC.h
@@ -0,0 +1,13 @@
+//
+//  VideoVC.h
+//  DevslopesTutorials
+//
+//  Created by Jacob Landman on 2/1/17.
+//  Copyright © 2017 Jacob Landman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VideoVC : UIViewController
+
+@end
---
+++ DevslopesTutorials/DevslopesTutorials/VideoVC.m
@@ -0,0 +1,37 @@
+//
+//  VideoVC.m
+//  DevslopesTutorials
+//
+//  Created by Jacob Landman on 2/1/17.
+//  Copyright © 2017 Jacob Landman. All rights reserved.
+//
+
+#import "VideoVC.h"
+
+@interface VideoVC ()
+
+@end
+
+@implementation VideoVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
